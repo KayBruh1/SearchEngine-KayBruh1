86cc0b946ef076d199f50cf017a24a05c7818cd5
KayBruh1 <kzahiri@dons.usfca.edu>
2024-02-21 17:22:43 -0800

    Introduce addWord method

--- src/main/java/edu/usfca/cs272/Driver.java
+++ src/main/java/edu/usfca/cs272/Driver.java
@@ -26,26 +26,26 @@ class InvertedIndex {
 
 	/** TreeMap storing inverted index for files and word positions */
 	public TreeMap<String, TreeMap<String, List<Integer>>> invertedIndex = new TreeMap<>();
-	
 
-    public void addWordCount(String filePath, Integer count) {
-        fileWordCounts.put(filePath, count);
-    }
-    
-    public void addWord(String word, String filePath, Integer position) {
-        if (!invertedIndex.containsKey(word)) {
-            invertedIndex.put(word, new TreeMap<>());
-        }
-        
-        TreeMap<String, List<Integer>> wordMap = invertedIndex.get(word);
 
-        if (!wordMap.containsKey(filePath)) {
-            wordMap.put(filePath, new ArrayList<>());
-        }
-        
-        List<Integer> wordPosition = wordMap.get(filePath);
-        wordPosition.add(position);
-    }
+	public void addWordCount(String filePath, Integer count) {
+		fileWordCounts.put(filePath, count);
+	}
+
+	public void addWord(String word, String filePath, Integer position) {
+		if (!invertedIndex.containsKey(word)) {
+			invertedIndex.put(word, new TreeMap<>());
+		}
+
+		TreeMap<String, List<Integer>> wordMap = invertedIndex.get(word);
+
+		if (!wordMap.containsKey(filePath)) {
+			wordMap.put(filePath, new ArrayList<>());
+		}
+
+		List<Integer> wordPosition = wordMap.get(filePath);
+		wordPosition.add(position);
+	}
 
 }
 
