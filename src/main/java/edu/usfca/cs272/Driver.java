a48924b18182e4d6278994a76da7dfd755e1b264
Sophie Engle <sjengle@cs.usfca.edu>
2024-03-09 13:01:44 -0800

    Declare variables where defined in Driver

--- src/main/java/edu/usfca/cs272/Driver.java
+++ src/main/java/edu/usfca/cs272/Driver.java
@@ -18,13 +18,13 @@ public class Driver {
 	 * @param args Command line arguments
 	 */
 	public static void main(String[] args) {
-		Path inputPath = null;
+		Path inputPath = null; // TODO Declare these where they are defined (inside the if statements)
 		String countsPath = null;
 		String indexPath = null;
 
 		ArgumentParser parser = new ArgumentParser(args);
 		InvertedIndex indexer = new InvertedIndex();
-		FileBuilder fileBuilder = new FileBuilder(indexer);
+		FileBuilder fileBuilder = new FileBuilder(indexer); // TODO Can declare this in if (-text) block too since not sued in multiple blocks
 
 		if (parser.hasFlag("-text")) {
 			inputPath = parser.getPath("-text");
@@ -52,5 +52,16 @@ public class Driver {
 				System.out.println("Error building the inverted index " + inputPath);
 			}
 		}
+		
+		/*
+		 * TODO The style of declaring all the variables at the start of code works when
+		 * code is small, but starts to be difficult to manage when code gets larger and
+		 * more complex. Try to declare the variables in the smallest scope possible and
+		 * maintain functionality. For example, for the paths, declare the variable
+		 * inside the if blocks where the path value is defined and used. The inverted
+		 * index data structure and argument parser are both used in multiple blocks, so
+		 * keep those declared at the start.
+		 */
+
 	}
 }
