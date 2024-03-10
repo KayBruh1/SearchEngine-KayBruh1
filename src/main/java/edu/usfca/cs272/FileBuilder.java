ee121a25ef7b8c3aed438e9524f3b3a09bb06f16
KayBruh1 <kzahiri@dons.usfca.edu>
2024-03-09 16:42:07 -0800

    Adjust isTextFile

--- src/main/java/edu/usfca/cs272/FileBuilder.java
+++ src/main/java/edu/usfca/cs272/FileBuilder.java
@@ -127,7 +127,7 @@ public class FileBuilder {
 	 * @param file The file to be checked
 	 * @return True for a valid file, false otherwise
 	 */
-	private static boolean isTextFile(Path file) { // TODO public
+	public static boolean isTextFile(Path file) {
 		String fileName = file.getFileName().toString().toLowerCase();
 		return Files.isRegularFile(file) && (fileName.endsWith(".txt") || fileName.endsWith(".text"));
 	}
