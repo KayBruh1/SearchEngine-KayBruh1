92b35f82b56a9a333a97c229408fd5922b68393b
KayBruh1 <kzahiri@dons.usfca.edu>
2024-03-09 16:39:59 -0800

    Adjust variable declarations

--- src/main/java/edu/usfca/cs272/Driver.java
+++ src/main/java/edu/usfca/cs272/Driver.java
@@ -18,16 +18,12 @@ public class Driver {
 	 * @param args Command line arguments
 	 */
 	public static void main(String[] args) {
-		Path inputPath = null; // TODO Declare these where they are defined (inside the if statements)
-		String countsPath = null;
-		String indexPath = null;
-
 		ArgumentParser parser = new ArgumentParser(args);
 		InvertedIndex indexer = new InvertedIndex();
-		FileBuilder fileBuilder = new FileBuilder(indexer); // TODO Can declare this in if (-text) block too since not sued in multiple blocks
 
 		if (parser.hasFlag("-text")) {
-			inputPath = parser.getPath("-text");
+			Path inputPath = parser.getPath("-text");
+			FileBuilder fileBuilder = new FileBuilder(indexer);
 			try {
 				fileBuilder.buildStructures(inputPath);
 			} catch (Exception e) {
@@ -36,32 +32,21 @@ public class Driver {
 		}
 
 		if (parser.hasFlag("-counts")) {
-			countsPath = parser.getString("-counts", ("counts.json"));
+			String countsPath = parser.getString("-counts", "counts.json");
 			try {
 				indexer.writeCounts(countsPath);
 			} catch (Exception e) {
-				System.out.println("Error building the file word counts " + inputPath);
+				System.out.println("Error building the file word counts " + countsPath);
 			}
 		}
 
 		if (parser.hasFlag("-index")) {
-			indexPath = parser.getString("-index", ("index.json"));
+			String indexPath = parser.getString("-index", "index.json");
 			try {
 				indexer.writeIndex(indexPath);
 			} catch (Exception e) {
-				System.out.println("Error building the inverted index " + inputPath);
+				System.out.println("Error building the inverted index " + indexPath);
 			}
 		}
-		
-		/*
-		 * TODO The style of declaring all the variables at the start of code works when
-		 * code is small, but starts to be difficult to manage when code gets larger and
-		 * more complex. Try to declare the variables in the smallest scope possible and
-		 * maintain functionality. For example, for the paths, declare the variable
-		 * inside the if blocks where the path value is defined and used. The inverted
-		 * index data structure and argument parser are both used in multiple blocks, so
-		 * keep those declared at the start.
-		 */
-
 	}
 }
