a70caeb05a04e8335e3ed544d5923fefa81b15b3
KayBruh1 <kzahiri@dons.usfca.edu>
2024-04-25 18:57:24 -0700

    Adjust status code checks

--- src/main/java/edu/usfca/cs272/WebCrawler.java
+++ src/main/java/edu/usfca/cs272/WebCrawler.java
@@ -5,8 +5,6 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
 
 public class WebCrawler {
 	private final HashSet<URI> visited;
@@ -18,27 +16,19 @@ public class WebCrawler {
 	public void crawl(URI uri, int redirects, ThreadSafeInvertedIndex indexer) throws IOException, URISyntaxException {
 		if (!visited.contains(uri)) {
 			visited.add(uri);
-			Map<String, List<String>> headers = HttpsFetcher.fetch(uri);
-			int statusCode = HtmlFetcher.getStatusCode(headers);
-			if (statusCode == 200 && HtmlFetcher.isHtml(headers)) {
-				String htmlContent = HtmlFetcher.fetch(uri, redirects);
-				if (htmlContent != null) {
-					String cleanedHtml = HtmlCleaner.stripHtml(htmlContent);
-					ArrayList<String> words = FileStemmer.listStems(cleanedHtml);
-					int position = 0;
-					for (String word : words) {
-						position++;
-						URI cleanURI = LinkFinder.clean(uri);
-						System.out.println(cleanURI.toString());
-						indexer.addWord(word, cleanURI.toString(), position);
-					}
-				}
-			} else if (statusCode >= 300 && statusCode <= 399 && redirects > 0) {
-				String redirect = HtmlFetcher.getRedirect(headers);
-				if (redirect != null) {
-					crawl(new URI(redirect), redirects - 1, indexer);
+			String htmlContent = HtmlFetcher.fetch(uri, redirects);
+			if (htmlContent != null) {
+				String cleanedHtml = HtmlCleaner.stripHtml(htmlContent);
+				ArrayList<String> words = FileStemmer.listStems(cleanedHtml);
+				int position = 0;
+				for (String word : words) {
+					position++;
+					URI cleanURI = LinkFinder.clean(uri);
+					System.out.println(cleanURI.toString());
+					indexer.addWord(word, cleanURI.toString(), position);
 				}
 			}
 		}
+
 	}
 }
