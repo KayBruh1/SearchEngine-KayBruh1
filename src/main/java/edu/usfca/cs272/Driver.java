e1066415de84f190194c17d550146494f2790a47
KayBruh1 <kzahiri@dons.usfca.edu>
2024-05-01 13:30:11 -0700

    Change duplicate code

--- src/main/java/edu/usfca/cs272/Driver.java
+++ src/main/java/edu/usfca/cs272/Driver.java
@@ -18,15 +18,10 @@ public class Driver {
 	 * @param args Command line arguments
 	 */
 	public static void main(String[] args) {
-		/*
-		 * TODO See if you can take advantage of inheritance and
-		 * upcasting to reduce the duplicate code in Driver
-		 * 
-		 * (Do this last, after the other changes)
-		 */
-		
-		
 		ArgumentParser parser = new ArgumentParser(args);
+		InvertedIndex indexer;
+		FileBuilder builder;
+		QueryFileProcessorInterface processor;
 
 		if (parser.hasFlag("-threads")) {
 			int numThreads = 5;
@@ -41,103 +36,58 @@ public class Driver {
 			}
 
 			CustomWorkQueue workQueue = new CustomWorkQueue(numThreads);
-			ThreadSafeInvertedIndex indexer = new ThreadSafeInvertedIndex();
-			ThreadedFileBuilder builder = new ThreadedFileBuilder(indexer, workQueue);
-			if (parser.hasFlag("-text")) {
-				Path inputPath = parser.getPath("-text");
-				try {
-					builder.buildStructures(inputPath);
-				} catch (Exception e) {
-					System.out.println("Error building the structures " + inputPath);
-				}
-
-			}
-
-			ThreadedQueryFileProcessor mtProcessor = new ThreadedQueryFileProcessor(indexer, workQueue,
+			indexer = new ThreadSafeInvertedIndex();
+			builder = new ThreadedFileBuilder((ThreadSafeInvertedIndex) indexer, workQueue);
+			processor = new ThreadedQueryFileProcessor((ThreadSafeInvertedIndex) indexer, workQueue,
 					parser.hasFlag("-partial"));
-			if (parser.hasFlag("-query")) {
-				Path queryPath = parser.getPath("-query");
-				try {
-					mtProcessor.processQueries(queryPath);
-				} catch (Exception e) {
-					System.out.println("Error reading the query file " + queryPath);
-				}
-			}
-			workQueue.shutdown();
-
-			if (parser.hasFlag("-counts")) {
-				Path countsPath = parser.getPath("-counts", Path.of("counts.json"));
-				try {
-					indexer.writeCounts(countsPath);
-				} catch (Exception e) {
-					System.out.println("Error building the file word counts " + countsPath);
-				}
-			}
-
-			if (parser.hasFlag("-index")) {
-				Path indexPath = parser.getPath("-index", Path.of("index.json"));
-				try {
-					indexer.writeIndex(indexPath);
-				} catch (Exception e) {
-					System.out.println("Error building the inverted index " + indexPath);
-				}
-			}
-
-			if (parser.hasFlag("-results")) {
-				Path resultsPath = parser.getPath("-results", Path.of("results.json"));
-				try {
-					mtProcessor.writeResults(resultsPath);
-				} catch (Exception e) {
-					System.out.println("Error writing results to file " + resultsPath);
-				}
-			}
 		} else {
-			InvertedIndex indexer = new InvertedIndex();
-			FileBuilder fileBuilder = new FileBuilder(indexer);
-			if (parser.hasFlag("-text")) {
-				Path inputPath = parser.getPath("-text");
-				try {
-					fileBuilder.buildStructures(inputPath);
-				} catch (Exception e) {
-					System.out.println("Error building the structures " + inputPath);
-				}
-			}
+			indexer = new InvertedIndex();
+			builder = new FileBuilder(indexer);
+			processor = new QueryFileProcessor(indexer, parser.hasFlag("-partial"));
+		}
 
-			QueryFileProcessor processor = new QueryFileProcessor(indexer, parser.hasFlag("-partial"));
-			if (parser.hasFlag("-query")) {
-				Path queryPath = parser.getPath("-query");
-				try {
-					processor.processQueries(queryPath);
-				} catch (Exception e) {
-					System.out.println("Error reading the query file " + queryPath);
-				}
+		if (parser.hasFlag("-text")) {
+			Path inputPath = parser.getPath("-text");
+			try {
+				builder.buildStructures(inputPath);
+			} catch (Exception e) {
+				System.out.println("Error building the structures " + inputPath);
 			}
+		}
 
-			if (parser.hasFlag("-counts")) {
-				Path countsPath = parser.getPath("-counts", Path.of("counts.json"));
-				try {
-					indexer.writeCounts(countsPath);
-				} catch (Exception e) {
-					System.out.println("Error building the file word counts " + countsPath);
-				}
+		if (parser.hasFlag("-query")) {
+			Path queryPath = parser.getPath("-query");
+			try {
+				((ThreadedQueryFileProcessor) processor).processQueries(queryPath); // Upcast here
+			} catch (Exception e) {
+				System.out.println("Error reading the query file " + queryPath);
 			}
+		}
 
-			if (parser.hasFlag("-index")) {
-				Path indexPath = parser.getPath("-index", Path.of("index.json"));
-				try {
-					indexer.writeIndex(indexPath);
-				} catch (Exception e) {
-					System.out.println("Error building the inverted index " + indexPath);
-				}
+		if (parser.hasFlag("-counts")) {
+			Path countsPath = parser.getPath("-counts", Path.of("counts.json"));
+			try {
+				indexer.writeCounts(countsPath);
+			} catch (Exception e) {
+				System.out.println("Error building the file word counts " + countsPath);
 			}
+		}
 
-			if (parser.hasFlag("-results")) {
-				Path resultsPath = parser.getPath("-results", Path.of("results.json"));
-				try {
-					processor.writeResults(resultsPath);
-				} catch (Exception e) {
-					System.out.println("Error writing results to file " + resultsPath);
-				}
+		if (parser.hasFlag("-index")) {
+			Path indexPath = parser.getPath("-index", Path.of("index.json"));
+			try {
+				indexer.writeIndex(indexPath);
+			} catch (Exception e) {
+				System.out.println("Error building the inverted index " + indexPath);
+			}
+		}
+
+		if (parser.hasFlag("-results")) {
+			Path resultsPath = parser.getPath("-results", Path.of("results.json"));
+			try {
+				processor.writeResults(resultsPath);
+			} catch (Exception e) {
+				System.out.println("Error writing results to file " + resultsPath);
 			}
 		}
 	}
