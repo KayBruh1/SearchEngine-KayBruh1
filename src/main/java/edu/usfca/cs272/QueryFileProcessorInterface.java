a0321a02799915c6755b081fd493a2482bacf1c9
KayBruh1 <kzahiri@dons.usfca.edu>
2024-05-03 11:41:18 -0700

    Fix stemmer use

--- src/main/java/edu/usfca/cs272/QueryFileProcessorInterface.java
+++ src/main/java/edu/usfca/cs272/QueryFileProcessorInterface.java
@@ -8,19 +8,17 @@ import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
-import opennlp.tools.stemmer.snowball.SnowballStemmer;
-
 /**
  * Interface for query processing
  */
 public interface QueryFileProcessorInterface {
-    /**
-     * Processes search queries from a path
-     *
-     * @param queryPath The path containing search queries
-     * @throws IOException If an I/O error occurs
-     */
-	default void processQueries(Path queryPath) throws IOException { // TODO Fix indentation
+	/**
+	 * Processes search queries from a path
+	 *
+	 * @param queryPath The path containing search queries
+	 * @throws IOException If an I/O error occurs
+	 */
+	default void processQueries(Path queryPath) throws IOException {
 		try (BufferedReader reader = Files.newBufferedReader(queryPath)) {
 			String line;
 			while ((line = reader.readLine()) != null) {
@@ -29,70 +27,64 @@ public interface QueryFileProcessorInterface {
 		}
 	}
 
-    /**
-     * Processes a single search query line
-     *
-     * @param queryLine The query line to process
-     */
-    void processQueries(String queryLine);
+	/**
+	 * Processes a single search query line
+	 *
+	 * @param queryLine The query line to process
+	 */
+	void processQueries(String queryLine);
 
-    /**
-     * Process query line to a stemmed query
-     *
-     * @param queryLine The query line to process
-     * @return The stemmed query
-     */
-    default String processQueryLine(String queryLine) {
-    	/* TODO Can use the other form of uniqueStems to reduce lines of code:
+	/**
+	 * Process query line to a stemmed query
+	 *
+	 * @param queryLine The query line to process
+	 * @return The stemmed query
+	 */
+	default String processQueryLine(String queryLine) {
 		TreeSet<String> query = FileStemmer.uniqueStems(queryLine);
 		return String.join(" ", query);
-		*/
-    	
-    	SnowballStemmer stemmer = new SnowballStemmer(SnowballStemmer.ALGORITHM.ENGLISH);
-		TreeSet<String> query = FileStemmer.uniqueStems(queryLine, stemmer);
-		return String.join(" ", query);
-    }
+	}
 
-    /**
-     * Checks if search results exist for a query
-     *
-     * @param queryLine The query to check results for
-     * @return True if search results exist, false otherwise
-     */
-    boolean hasSearchResults(String queryLine);
-    
-    /**
-     * Gets the search results for a query line
-     *
-     * @param queryLine The query line for search results
-     * @return The search results for the query line
-     */
-    List<InvertedIndex.SearchResult> getQueryLineResults(String queryLine);
+	/**
+	 * Checks if search results exist for a query
+	 *
+	 * @param queryLine The query to check results for
+	 * @return True if search results exist, false otherwise
+	 */
+	boolean hasSearchResults(String queryLine);
 
-    /**
-     * Returns the total number processed queries
-     *
-     * @return The total number of processed queries
-     */
-    /*
-     * TODO Make this a default method using your view...
-     * 
-     * return viewQueryResults().size();
-     */
-    int getTotalQueries();
-    
-    /**
-     * Returns an unmodifiable set containing the queries for search results
-     *
-     * @return An unmodifiable set containing the queries for search results
-     */
-    Set<String> viewQueryResults();
+	/**
+	 * Gets the search results for a query line
+	 *
+	 * @param queryLine The query line for search results
+	 * @return The search results for the query line
+	 */
+	List<InvertedIndex.SearchResult> getQueryLineResults(String queryLine);
 
-    /**
-     * Writes the search results to a JSON file
-     *
-     * @param resultsPath the output path of the JSON file
-     * @throws IOException if an I/O error occurs
-     */
-    void writeResults(Path resultsPath) throws IOException;
+	/**
+	 * Returns the total number processed queries
+	 *
+	 * @return The total number of processed queries
+	 */
+	/*
+	 * TODO Make this a default method using your view...
+	 * 
+	 * return viewQueryResults().size();
+	 */
+	int getTotalQueries();
+
+	/**
+	 * Returns an unmodifiable set containing the queries for search results
+	 *
+	 * @return An unmodifiable set containing the queries for search results
+	 */
+	Set<String> viewQueryResults();
+
+	/**
+	 * Writes the search results to a JSON file
+	 *
+	 * @param resultsPath the output path of the JSON file
+	 * @throws IOException if an I/O error occurs
+	 */
+	void writeResults(Path resultsPath) throws IOException;
 }
